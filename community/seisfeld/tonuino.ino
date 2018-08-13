3f862b5e29280625254939d89e3b4e5c494b3e6b
Stephan Eisfeld <github@eisfeld.ws>
2018-08-13 20:12:27 +0200

    replaces jukebox with TonUINO in comments

--- community/seisfeld/tonuino.ino
+++ community/seisfeld/tonuino.ino
@@ -30,7 +30,7 @@
   ----------
 
   If a TSOP38238 is connected to pin 5, you can also use an ir remote to remote
-  control the jukebox. There are code mappings for the silver apple remote below, but you can
+  control TonUINO. There are code mappings for the silver apple remote below, but you can
   change them to other codes to match different remotes. This feature can be disabled by
   commenting a define below, to save some memory. The actions are as follows:
 
@@ -55,7 +55,7 @@
   -----------
 
   If a led is connected to pin 6, limited status information is given using that led.
-  The led is solid on when the jukebox is running (ie. has power and got initialized). The
+  The led is solid on when TonUINO is running (ie. has power and got initialized). The
   led is pulsing slowly to indicate playback. When the box is in setup new nfc tag or erase
   nfc tag mode, the led is blinking every 500ms. This feature can be disabled by commenting
   a define below, to save some memory.
@@ -488,7 +488,7 @@ void fadeStatusLed(bool isPlaying) {
   static int16_t statusLedValue = 255;
   static uint32_t statusLedOldMillis;
 
-  // jukebox is playing, fade status led in or out
+  // TonUINO is playing, fade status led in or out
   if (isPlaying) {
     uint32_t statusLedNewMillis = millis();
     if (statusLedNewMillis - statusLedOldMillis >= 100) {
@@ -510,7 +510,7 @@ void fadeStatusLed(bool isPlaying) {
       analogWrite(statusLedPin, statusLedValue);
     }
   }
-  // jukebox is not playing, set to full brightness
+  // TonUINO is not playing, set to full brightness
   else {
     statusLedValue = 255;
     analogWrite(statusLedPin, statusLedValue);
