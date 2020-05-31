a5ba83f693bf515db0021fec1567d0055adf526a
GuteLaune <65677822+GuteLaune@users.noreply.github.com>
2020-05-31 02:12:08 +0200

    bigfix issue #66
    Fehler bei "Titel wiederholen" im Partymodus behoben

--- Tonuino.ino
+++ Tonuino.ino
@@ -458,7 +458,12 @@ class RepeatSingleModifier: public Modifier {
       Serial.println(F("== RepeatSingleModifier::handleNext() -> REPEAT CURRENT TRACK"));
       delay(50);
       if (isPlaying()) return true;
-      mp3.playFolderTrack(myFolder->folder, currentTrack);
+      if (myFolder->mode == 3 || myFolder->mode == 9){
+        mp3.playFolderTrack(myFolder->folder, queue[currentTrack - 1]);
+      }
+      else{
+        mp3.playFolderTrack(myFolder->folder, currentTrack);
+      }
       _lastTrackFinished = 0;
       return true;
     }
