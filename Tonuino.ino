ac8f4909e9dff2fa154b9eb393abfdb7cca26973
Franz-Xaver Geiger <mail@fx-g.de>
2020-11-26 20:00:44 +0100

    Fix warning: default argument given for parameter
    Defaultargumente sind nur in der ersten Deklaration einer
    Funktionssignatur erlaubt. Falls eine Funktion erst deklariert und
    weiter unten erst implementiert wird, darf das Defaultargument nicht
    wiederholt werden. Ansonsten wird die beschriebene Warnung ausgegeben
    was dazu führt, dass ggf. wichtigere Meldungen zwischen den Warnungen
    verloren gehen.
    
    Die Warnung kann beseitigt werden indem die Wiederholung der
    Defaultargumente entfernt wird. Das geschieht in dieser Änderung für die
    beiden Funktionen `adminMenu()` und `voiceMenu()`.

--- Tonuino.ino
+++ Tonuino.ino
@@ -1114,7 +1114,7 @@ void loop() {
   mfrc522.PCD_StopCrypto1();
 }
 
-void adminMenu(bool fromCard = false) {
+void adminMenu(bool fromCard) {
   disablestandbyTimer();
   mp3.pause();
   Serial.println(F("=== adminMenu()"));
@@ -1346,7 +1346,7 @@ bool askCode(uint8_t *code) {
 }
 
 uint8_t voiceMenu(int numberOfOptions, int startMessage, int messageOffset,
-                  bool preview = false, int previewFromFolder = 0, int defaultValue = 0, bool exitWithLongPress = false) {
+                  bool preview, int previewFromFolder, int defaultValue, bool exitWithLongPress) {
   uint8_t returnValue = defaultValue;
   if (startMessage != 0)
     mp3.playMp3FolderTrack(startMessage);
